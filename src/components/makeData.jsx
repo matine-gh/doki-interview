2186894969527be0abbcca66d301f5c27779e69f
matine-gh <matine.ghazanfari@gmail.com>
2023-07-29 18:24:46 +0330

    [fix]-comment makeData function and use static data

--- src/components/makeData.jsx
+++ src/components/makeData.jsx
@@ -1,4 +1,385 @@
 
+
+const fake = [
+    {
+        age: 1,
+        progress: "1",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "10",
+    },
+    {
+        age: 2,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "20",
+    },
+    {
+        age: 3,
+        progress: "3",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 4,
+        progress: "4",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "40",
+    },
+    {
+        age: 5,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "50",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "single",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "single",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "single",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "single",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "single",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "rrr",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "rrr",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "rrr",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "rrr",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 6,
+        progress: "6",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 7,
+        progress: "7",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "70",
+    },
+    {
+        age: 4,
+        progress: "5",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "60",
+    },
+    {
+        age: 8,
+        progress: "8",
+        status: "complicated",
+        subRows: "undefined",
+        visits: "80",
+    },
+    {
+        age: 9,
+        progress: "9",
+        status: "single",
+        subRows: "undefined",
+        visits: "90",
+    },
+    {
+        age: 1,
+        progress: "2",
+        status: "single",
+        subRows: "undefined",
+        visits: "30",
+    },
+    {
+        age: 8,
+        progress: "7",
+        status: "single",
+        subRows: "undefined",
+        visits: "60",
+    },
+];
 const range = len => {
     const arr = []
     for (let i = 0; i < len; i++) {
@@ -23,16 +404,18 @@ const newPerson = () => {
 }
 
 export default function makeData(...lens) {
-    const makeDataLevel = (depth = 0) => {
-        const len = lens[depth]
-        return range(len).map(d => {
-            return {
-                ...newPerson(),
-                subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-            }
-        })
-    }
+    // const makeDataLevel = (depth = 0) => {
+    //     const len = lens[depth]
+    //     return range(len).map(d => {
+    //         return {
+    //             ...newPerson(),
+    //             subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+    //         }
+    //     })
+    // }
+    //
+    // return makeDataLevel()
 
-    return makeDataLevel()
+    return fake;
 }
 
